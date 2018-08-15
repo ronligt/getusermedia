acd0b9d149bb55bb0335ccdacd55d4b5fe0c0fde
Ronald Ligteringen <r.ligteringen@gmail.com>
2018-08-15 15:01:39 +0200

    first setup (removed jquery)

--- promise.js
+++ promise.js
@@ -1,6 +1,28 @@
-$("#start").click(function(){
-    alert("Start");
-});
-$("#stop").click(function(){
+function record() {
+
+    navigator.mediaDevices.getUserMedia({audio: true})
+        .then(stream => {
+            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+        });
+}
+
+
+// Main code
+
+startButton = document.getElementById("start");
+stopButton = document.getElementById("stop");
+
+startButton.disabled = false;
+
+startButton.onclick = function(){
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    record();
+};
+
+stopButton.onclick = function(){
+    stopButton.disabled = true;
+    startButton.disabled = false;
     alert("Stop");
-});
+};
