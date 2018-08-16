7e4f7da1effbd3fc14608aba56dbd2d0f6e696ef
Ronald Ligteringen <r.ligteringen@gmail.com>
2018-08-16 10:08:02 +0200

    found audio-data! Using createScriptProcessor and getChannelData

--- promise.js
+++ promise.js
@@ -1,13 +1,25 @@
 function record() {
-
-    navigator.mediaDevices.getUserMedia({audio: true})
-        .then(stream => {
-            const mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
+    if (navigator.mediaDevices) {
+        console.log('we have media devices');
+        navigator.mediaDevices.getUserMedia({audio: true, video: false})
+        .then(function(stream) {
+            var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+            var source = audioCtx.createMediaStreamSource(stream);
+            var processor = audioCtx.createScriptProcessor(1024, 1, 1);
+            source.connect(processor);
+            processor.connect(audioCtx.destination);
+            processor.onaudioprocess = function(e) {
+                console.log(e.inputBuffer);
+                console.log(e.inputBuffer.getChannelData(0));
+            };
+        })
+        .catch(function(err) {
+            console.log('Error: ' + err);
         });
+    } else {
+        console.log('Nope, no media devices!');
+    }
 }
-
-
 // Main code
 
 startButton = document.getElementById("start");
@@ -18,7 +30,7 @@ startButton.disabled = false;
 startButton.onclick = function(){
     startButton.disabled = true;
     stopButton.disabled = false;
-    record();
+    recording();
 };
 
 stopButton.onclick = function(){
